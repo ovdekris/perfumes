6e1828dada793b623b2d1e8bf11661dfd045f9f8
ovdekris <ovde.kris@gmail.com>
2023-05-30 20:58:45 +0200

    Styling modeule header for infromation about delivery

--- src/MainPage/Module/Module.js
+++ src/MainPage/Module/Module.js
@@ -1,10 +1,10 @@
 import style from "./Module.module.css";
-import {useState} from "react";
-function Module({open},{setOpenModule}){
-    console.log({open}, {setOpenModule});
+function Module({setOpenModule}){
     return(
         <div className={style.container}>
+            <div className={style.description}>Free delivery from 200 zł!</div>
             <button className={style.button} onClick={()=>{
+                    setOpenModule(false)
             }}>
                 Button
             </button>
