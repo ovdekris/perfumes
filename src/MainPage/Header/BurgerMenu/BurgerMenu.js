f6f7562d1c9c99866d3d2e58d9f2269afefbf3e9
ovdekris <ovde.kris@gmail.com>
2023-07-12 10:29:16 +0200

    Styling burger

--- src/MainPage/Header/BurgerMenu/BurgerMenu.js
+++ src/MainPage/Header/BurgerMenu/BurgerMenu.js
@@ -5,7 +5,7 @@ function BurgerMenu({active, setActive}){
          <div className={active? 'container active' :'container'}>
              <div className="blur">
                  <div className="container__menu">
-
+                    <div onClick={()=>{setActive(!active)}}>button</div>
                  </div>
              </div>
          </div>
