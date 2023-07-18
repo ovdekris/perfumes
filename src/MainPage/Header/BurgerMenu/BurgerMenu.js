3ffc6eb8f4b680cca5d934c3cea631f086495f4a
ovdekris <ovde.kris@gmail.com>
2023-07-18 19:15:24 +0200

    Adaptive burger

--- src/MainPage/Header/BurgerMenu/BurgerMenu.js
+++ src/MainPage/Header/BurgerMenu/BurgerMenu.js
@@ -1,12 +1,37 @@
 import  "./BurgerMenu.Module.css";
+import style from "../Header.module.css";
+import React from "react";
 function BurgerMenu({active, setActive}){
     return(
         <>
          <div className={active? 'container active' :'container'}>
              <div className="blur">
                  <div className="container__menu">
-                    <div onClick={()=>{setActive(!active)}}>button</div>
-                 </div>
+
+                         <div>
+                             <ul>
+                                 <li>
+                                     <a href="">Sale</a>
+                                 </li>
+                                 <li>
+                                     <a href="">For men</a>
+                                 </li>
+                                 <li>
+                                     <a href="">For women</a>
+                                 </li>
+                                 <li>
+                                     <a href="">Body mist</a>
+                                 </li>
+                                 <li>
+                                     <a href="">Home fragrances</a>
+                                 </li>
+                                 <li>
+                                     <a href="">Signature</a>
+                                 </li>
+                             </ul>
+                         </div>
+                     <div onClick={()=>{setActive(!active)}}>button</div>
+                     </div>
              </div>
          </div>
         </>
