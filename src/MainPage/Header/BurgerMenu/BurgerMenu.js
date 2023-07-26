4be371faea43f0ee42473ca24022a1e0267b9233
ovdekris <ovde.kris@gmail.com>
2023-07-26 09:26:49 +0200

    Styling burger

--- src/MainPage/Header/BurgerMenu/BurgerMenu.js
+++ src/MainPage/Header/BurgerMenu/BurgerMenu.js
@@ -7,30 +7,38 @@ function BurgerMenu({active, setActive}){
          <div className={active? 'container active' :'container'}>
              <div className="blur">
                  <div className="container__menu">
-
-                         <div>
-                             <ul>
+                     <div className="content__menu__button">
+                         <div className="content__menu__title">Perfumy</div>
+                     <div onClick={()=>{setActive(!active)}} className="button__burger"></div>
+                     </div>
+                         <div className="content__menu__list">
+                             <ul className="content__menu__list__ul">
                                  <li>
                                      <a href="">Sale</a>
                                  </li>
+                                 <hr/>
                                  <li>
                                      <a href="">For men</a>
                                  </li>
+                                 <hr/>
                                  <li>
                                      <a href="">For women</a>
                                  </li>
+                                 <hr/>
                                  <li>
                                      <a href="">Body mist</a>
                                  </li>
+                                 <hr/>
                                  <li>
                                      <a href="">Home fragrances</a>
                                  </li>
+                                 <hr/>
                                  <li>
                                      <a href="">Signature</a>
                                  </li>
+                                 <hr/>
                              </ul>
                          </div>
-                     <div onClick={()=>{setActive(!active)}}>button</div>
                      </div>
              </div>
          </div>
