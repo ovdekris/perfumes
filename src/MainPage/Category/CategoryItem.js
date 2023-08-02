72a605391bfa5d1aadbe1ad2fa835ba5afdf7843
ovdekris <ovde.kris@gmail.com>
2023-08-02 09:05:34 +0200

    Adaptive slider and category

--- src/MainPage/Category/CategoryItem.js
+++ src/MainPage/Category/CategoryItem.js
@@ -3,6 +3,7 @@ import image from "./img1.jpg";
 function CategoryItem(){
     return(
         <div className={style.container}>
+            <img src={image} alt="image" className={style.image}/>
            <div className={style.title}>For Woman</div>
         </div>
     )
