e905dd5e4bf246da48ccd1df084317b2a75510bc
ovdekris <ovde.kris@gmail.com>
2023-08-04 10:00:35 +0200

    Styling bestseller section

--- src/MainPage/Bestseller/BestsellerItem.js
+++ src/MainPage/Bestseller/BestsellerItem.js
@@ -7,9 +7,9 @@ function BestsellerItem(){
            <div className={style.image}>
                <img src={image} alt="image"/>
            </div>
-           <div>
-               <div className={style.title}>TItle</div>
-               <div className={style.description}>Description</div>
+           <div className={style.description__container}>
+               <div className={style.title}>Paco Rabanne 1 million</div>
+               <div className={style.description}>1 Million od Paco Rabanne to zapach dla szarmanckich mężczyzn, którzy ochoczo stawiają wszystko na jedną kartę. Kompozycja zapachowa przywodzi na myśl śmialą grę hazardową, w której chodzi tylko o jedno: Złoto! </div>
                <div className={style.button}>Button</div>
            </div>
            </div>
