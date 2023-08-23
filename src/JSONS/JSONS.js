8071b35a4097493758251a194fdc839174e4d678
ovdekris <ovde.kris@gmail.com>
2023-08-23 11:33:54 +0200

    Create a data

--- src/JSONS/JSONS.js
+++ src/JSONS/JSONS.js
@@ -4,20 +4,23 @@ import {useEffect, useState} from "react";
 function JSONS(){
     const [data,setData]=useState([]);
     useEffect(()=>{
-        axios.get("http://localhost:5000/perfume")
+        axios.get("http://localhost:5000/data")
             .then(res=>setData(res.data))
             .catch(er=>console.log(er))
     },[])
+    console.log(data.titles.map((d)=>{
+        console.log(d.title)
+    }));
     return(
         <div>
-            {data.map((d,i)=>
-            {
-                return (
-                    <div>
-                        {d.title}
-                    </div>
-                )
-            })}
+            {/*{data.map((d,i)=>*/}
+            {/*{*/}
+            {/*    return (*/}
+            {/*        <div>*/}
+            {/*            {d.title}*/}
+            {/*        </div>*/}
+            {/*    )*/}
+            {/*})}*/}
         </div>
     )
 }
