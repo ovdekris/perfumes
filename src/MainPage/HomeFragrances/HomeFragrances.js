c70150ab2863bb5faaddc4f47f86484cba35a0ea
ovdekris <ovde.kris@gmail.com>
2023-09-04 21:35:56 +0200

    Create a data structures of home fragrances

--- src/MainPage/HomeFragrances/HomeFragrances.js
+++ src/MainPage/HomeFragrances/HomeFragrances.js
@@ -7,10 +7,17 @@ function HomeFragrances({props}){
         <div>
             <Module/>
             <Header/>
-            <div className={style.content}>
-            {props.map(record => (
-                <CardProduct key={props.id} props={record} className={style.content__item}/>
-            ))}
+            <div className={style.container}>
+                <div className={style.container__title}>
+                    <div className={style.title__div}></div>
+                    Home Fragrances
+                    <div className={style.title__div}></div>
+                </div>
+                <div className={style.content}>
+                    {props.map(record => (
+                        <CardProduct key={props.id} props={record} className={style.content__item}/>
+                    ))}
+                </div>
             </div>
         </div>
     )
