dd3726ea082777f23adf14b28fe804a35c933246
ovdekris <ovde.kris@gmail.com>
2023-09-11 17:56:58 +0200

    Styling card of product

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,6 +1,7 @@
 import Header from "../MainPage/Header/Header";
 import Module from "../MainPage/Module/Module"
 import Footer from "../MainPage/Footer/Footer";
+import {AiOutlineStar,AiFillStar} from "react-icons/ai";
 import style from "./Product.module.css";
 function Product({props}){
     const data=props.forMen;
@@ -13,16 +14,25 @@ function Product({props}){
             <Header/>
             <div className={style.content}>
                 <div className={style.content__image}>
-                   <div className={style.content__img}></div>
+                   <div className={style.content__img}>
+                       <img src="https://assets.vogue.com/photos/6054ce48ba2b50871bd1d368/3:4/w_640,c_limit/slide_8.jpg" alt="image"/>
+                   </div>
                     <div className={style.content__image__description}>
                         <div className={style.content__title}>
-                            Chanel 5
+                            Dior Sauvage - pefume for Man
+                        </div>
+                        <div  className={style.content__range}>
+                            <AiFillStar className={style.content__range__item}/>
+                            <AiFillStar className={style.content__range__item}/>
+                            <AiOutlineStar className={style.content__range__item}/>
+                            <AiOutlineStar className={style.content__range__item}/>
+                            <AiOutlineStar className={style.content__range__item}/>
                         </div>
                         <div className={style.content__availabality}>
-                            Availabality: available
+                            Availabality:   available
                         </div>
                         <div className={style.content__price}>
-                            Price: 130$
+                            130$
                         </div>
                         <div className={style.content__count}>
                             Count
