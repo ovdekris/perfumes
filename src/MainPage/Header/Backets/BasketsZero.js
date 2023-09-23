533b2387bd844fa48491d59b3a3e1ec8b9021f83
ovdekris <ovde.kris@gmail.com>
2023-09-23 09:28:27 +0200

    Styling Basket Zero Card

--- src/MainPage/Header/Backets/BasketsZero.js
+++ src/MainPage/Header/Backets/BasketsZero.js
@@ -1,8 +1,13 @@
 import style from "./BasketZero.module.css";
+import {SlBasket} from "react-icons/sl"
 function BasketsZero({show,setShow}){
     return(
         <div style={{display: show? 'flex':'none'}} className={style.container}>
-            BasketZero
+            <div className={style.content}>
+                <SlBasket/>
+                Basket Empty
+                <button>shopping</button>
+            </div>
         </div>
     )
 }
