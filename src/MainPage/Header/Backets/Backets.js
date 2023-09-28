d7ddd7660796f50df0d1a333452247e5f1e24586
ovdekris <ovde.kris@gmail.com>
2023-09-28 11:04:39 +0200

    Create components basket

--- src/MainPage/Header/Backets/Backets.js
+++ src/MainPage/Header/Backets/Backets.js
@@ -1,11 +1,25 @@
 import Module from "../../Module/Module";
 import Header from "../Header";
+import {useContext} from "react";
+import {CustomContext} from "../../Context/Context";
+import CardProduct from "../../CardProduct/CardProduct";
+import style from "../../ForWoman/ForWoman.module.css";
+import BasketsProduct from "./BasketsProduct";
 function Backets(){
+    const {basket}=useContext(CustomContext)
     return(
         <div>
             <Module/>
             <Header/>
-            Backets
+            <div>
+                Your Baskets
+            </div>
+            <div>
+                Product
+                {basket.map(record => (
+                    <BasketsProduct key={record.id} props={record} className={style.content__item}/>
+                ))}
+            </div>
         </div>
     )
 }
---
+++ src/MainPage/Header/Backets/BasketsProduct.js
@@ -0,0 +1,16 @@
+
+function BasketsProduct({props}){
+    console.log(props)
+    return(
+        <div>
+            <div>
+                image
+            </div>
+            <div>
+                description
+            </div>
+        </div>
+    )
+}
+
+export default BasketsProduct;
