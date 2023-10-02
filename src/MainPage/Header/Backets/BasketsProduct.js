44cf2f4ea47bdaa5287d4bc3cbb7764cdca103fd
ovdekris <ovde.kris@gmail.com>
2023-10-02 23:04:02 +0200

    Styling basket

--- src/MainPage/Header/Backets/BasketsProduct.js
+++ src/MainPage/Header/Backets/BasketsProduct.js
@@ -1,21 +1,22 @@
 import style from "./BasketsProduct.module.css"
 function BasketsProduct({props}){
+    console.log(props)
     return(
         <div className={style.content__product}>
             <div className={style.content__image}>
                 <img src={props.image} alt="Image"/>
             </div>
             <div className={style.content__product__description}>
-                description
+                {props.title}, {props.scents} ,
             </div>
             <div className={style.content__product__description__price}>
-                price
+                {props.price}
             </div>
             <div className={style.content__product__description__count}>
-                count
+                {props.count}
             </div>
             <div className={style.content__product__description__price__count}>
-                price count
+                {props.count*props.price}
             </div>
             <div className={style.content__product__description__delete}>
                 delete
