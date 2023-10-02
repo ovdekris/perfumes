a045751a37c2a220e4342954c3f1faccd5497cc8
ovdekris <ovde.kris@gmail.com>
2023-10-02 21:01:40 +0200

    Styling basket card

--- src/MainPage/Header/Backets/BasketsProduct.js
+++ src/MainPage/Header/Backets/BasketsProduct.js
@@ -1,13 +1,26 @@
+import style from "./BasketsProduct.module.css"
 function BasketsProduct({props}){
     return(
-        <div>
-            <div>
-                image
+        <div className={style.content__product}>
+            <div className={style.content__image}>
+                <img src={props.image} alt="Image"/>
             </div>
-            <div>
+            <div className={style.content__product__description}>
                 description
             </div>
-        </div>
+            <div className={style.content__product__description__price}>
+                price
+            </div>
+            <div className={style.content__product__description__count}>
+                count
+            </div>
+            <div className={style.content__product__description__price__count}>
+                price count
+            </div>
+            <div className={style.content__product__description__delete}>
+                delete
+            </div>
+            </div>
     )
 }
 
